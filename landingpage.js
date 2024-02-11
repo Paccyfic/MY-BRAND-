70ceb09ffdcf091a77880ccd1667ba727911ef7a
Ndahiro Pacific <--global>
2024-02-11 23:49:12 +0000

    added animations to the landing page.

---
+++ landingpage.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Front-end Developer', 'UI/UX Designer', 'Blogger.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
