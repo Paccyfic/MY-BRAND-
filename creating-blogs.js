d90594b1024b4f62f989cbdf622ffb6f81132396
Paccyfic <ndahiropaccyfic@gmail.com>
2024-03-07 22:25:39 +0200

    added integration between the registered-users dashboard and the back-end.

--- creating-blogs.js
+++ creating-blogs.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', async function (e) {
         const formData = new FormData(form);
 
         try {
-            const response = await fetch('http://localhost:3000/', {
+            const response = await fetch('http://localhost:3000', {
                 method: 'POST',
                 body: formData,
             });
