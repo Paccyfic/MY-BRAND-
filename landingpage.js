7072f46b99b64428b7992f632514a87130493d8a
Paccyfic <ndahiropaccyfic@gmail.com>
2024-03-23 19:29:24 +0200

    Added animations to the home page.

--- landingpage.js
+++ landingpage.js
@@ -1,7 +1,7 @@
 const typed = new Typed('.multiple-text', {
     strings: ['Front-end Developer', 'UI/UX Designer', 'Blogger.'],
-    typeSpeed: 150,
-    backSpeed: 150,
+    typeSpeed: 50,
+    backSpeed: 80,
     backDelay: 1000,
     loop: true
 });
